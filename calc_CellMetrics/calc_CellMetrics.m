a9e63bc2719298ebd41ea1097364a11814901979
petersenpeter <petersen.peter@gmail.com>
2020-01-28 14:13:50 -0500

    Update calc_CellMetrics.m

--- calc_CellMetrics/calc_CellMetrics.m
+++ calc_CellMetrics/calc_CellMetrics.m
@@ -1227,8 +1227,8 @@ cell_metrics_type_cell = {'brainRegion','animal','sex','species','strain','genet
 cell_metrics_type_numeric = {'spikeGroup','spikeCount','firingRate','cv2','refractoryPeriodViolation','burstIndex_Mizuseki2012','thetaModulationIndex','acg_tau_rise',...
     'acg_tau_decay','acg_tau_burst','acg_refrac','acg_fit_rsquare','burstIndex_Royer2012','burstIndex_Doublets','synapticConnectionsIn','synapticConnectionsOut','maxWaveformCh',...
     'maxWaveformCh1','troughToPeak','ab_ratio','peakVoltage','isolationDistance','lRatio','ripples_modulationIndex','ripples_modulationPeakResponseTime','deepSuperficialDistance',...
-    'thetaPhasePeak','thetaPhaseTrough','thetaEntrainment','thetaModulationIndex','spatialCoverageIndex','spatialGiniCoeff','spatialCoherence','spatialPeakRate',...
-    'placeFieldsCount','placeCell','firingRateGiniCoeff','firingRateStd','firingRateInstability'};
+    'thetaPhasePeak','thetaPhaseTrough','thetaEntrainment','thetaModulationIndex','spatialCoverageIndex','spatialGiniCoeff','spatialCoherence','spatialPeakRate', 'placeFieldsCount',...
+    'placeCell','firingRateGiniCoeff','firingRateStd','firingRateInstability'};
 
 cell_metrics_fieldnames = fieldnames(cell_metrics);
 cell_metrics_types = struct2cell(structfun(@class,cell_metrics,'UniformOutput',false));
@@ -1239,23 +1239,21 @@ cell_metrics_cell = find(ismember(cell_metrics_types,{'cell'}));
 fields_struct = find(ismember(cell_metrics_types,'struct'));
 
 % Verifying struct type
-if ~any(ismember(cell_metrics_fieldnames(fields_struct),cell_metrics_type_struct))
-    error(['cell_metrics field not formatted correctly'])
+if ~any(ismember(cell_metrics_types((ismember(cell_metrics_fieldnames,cell_metrics_type_struct))),'struct'))
+    error(['struct field not formatted correctly in cell_metrics'])
 end
 % Verifying numeric type
-if ~any(ismember(cell_metrics_fieldnames(cell_metrics_numeric),cell_metrics_type_numeric))
-    error(['cell_metrics field not formatted correctly'])
+if ~any(ismember(cell_metrics_types((ismember(cell_metrics_fieldnames,cell_metrics_type_numeric))),'double'))
+    error(['numeric field not formatted correctly in cell_metrics'])
 end
 % Verifying cell type
-if ~any(ismember(cell_metrics_fieldnames(cell_metrics_cell),cell_metrics_type_cell))
-    error(['cell_metrics field not formatted correctly'])
+if ~any(ismember(cell_metrics_types((ismember(cell_metrics_fieldnames,cell_metrics_type_cell))),'cell'))
+    error(['cell array field not formatted correctly in cell_metrics'])
 end
-
 % Verifying field sizes
 if any(cell_metrics_sizes(cell_metrics_numeric_cell,1) ~= 1) || any(cell_metrics_sizes(cell_metrics_numeric_cell,2) ~= cell_metrics.general.cellCount)
     error(['cell_metrics field not formatted correctly'])
 end
-
 % Verifying struct fields
 for i = 1:length(fields_struct)
     if ~strcmp(cell_metrics_fieldnames{fields_struct(i)},{'general','putativeConnections'})
