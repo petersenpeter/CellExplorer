5ad07af1249c440a5cafe7c791d98d3183f1cc1b
petersenpeter <petersen.peter@gmail.com>
2020-03-26 19:25:49 -0400

    Updates to spike plots and customSpikesPlots

--- +customSpikesPlots/spikes_RFheatStimulation.m
+++ +customSpikesPlots/spikes_RFheatStimulation.m
@@ -1,4 +1,4 @@
-function spikePlot = spikes_pulses
+function spikePlot = spikes_RFheatStimulation
 
 spikePlot.x = 'times';
 spikePlot.y = 'times';
@@ -8,15 +8,15 @@ spikePlot.state = '';
 spikePlot.filter = '';
 spikePlot.filterType = '';                 % [none, equal to, less than, greater than]
 spikePlot.filterValue = 0;
-spikePlot.event = 'pulses';
-spikePlot.eventType = 'manipulation';      % [events,manipulation,states]
+spikePlot.event = 'RFheatStimulation';
+spikePlot.eventType = 'events';            % [events,manipulation,states]
 spikePlot.eventAlignment = 'onset';        % [onset, offset, center, peak]
-spikePlot.eventSorting = 'none';           % [none, time, amplitude, duration]
-spikePlot.eventSecBefore = 0.2;            % in seconds
-spikePlot.eventSecAfter = 0.1;             % in seconds
+spikePlot.eventSorting = 'amplitude';      % [none, time, amplitude, duration]
+spikePlot.eventSecBefore = 3;              % in seconds
+spikePlot.eventSecAfter = 7;               % in seconds
 spikePlot.plotRaster = 1;
 spikePlot.plotAverage = 1;
-spikePlot.plotAmplitude = 1;
+spikePlot.plotAmplitude = 0;
 spikePlot.plotDuration = 0;
 spikePlot.plotCount = 0;
 
