0b248fc828d98c2e7b1757c6dfd50d18c0c53f31
Peter C. Petersen <petersen.peter@gmail.com>
2022-04-13 20:40:58 +0200

    Update NeuroScope2.m

--- NeuroScope2.m
+++ NeuroScope2.m
@@ -3009,7 +3009,7 @@ end
                     uiresume(UI.fig);
                 elseif UI.uitabgroup_channels.Selection == 4 && isfield(data.session.extracellular,'chanCoords') && ~isempty(data.session.extracellular.chanCoords.x) && ~isempty(data.session.extracellular.chanCoords.y)
                     image_toolbox_installed = isToolboxInstalled('Image Processing Toolbox');
-                    if ~verLessThan('matlab', '9.4') & image_toolbox_installed
+                    if ~verLessThan('matlab', '9.5') & image_toolbox_installed
                         x_lim_data = [min(data.session.extracellular.chanCoords.x),max(data.session.extracellular.chanCoords.x)];
                         y_lim_data = [min(data.session.extracellular.chanCoords.y),max(data.session.extracellular.chanCoords.y)];
                         x_padding = 0.03*diff(x_lim_data);
