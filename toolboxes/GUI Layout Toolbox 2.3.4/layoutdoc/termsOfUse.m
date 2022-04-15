06588975a18191e7d0d8beec225ae0302a1e2cf7
Peter Petersen <petersen.peter@gmail.com>
2022-04-15 13:11:56 +0200

    Updating GUI Layout Toolbox from version 2.3.4 to 2.3.5 to have compatibility with KiloSort GUI

--- toolboxes/GUI Layout Toolbox 2.3.4/layoutdoc/termsOfUse.m
+++
@@ -1,8 +0,0 @@
-function termsOfUse
-    if verLessThan('matlab','8.5')
-        helpview([matlabroot,'/license.txt'])
-    else
-        helpview([matlabroot,'/license_agreement.txt'])
-    end
-end
-
