ab1d2207de9fe6b16438cf0996aaf54783a62ddb
lkaraba <karaba24@gmail.com>
2022-10-10 14:13:41 -0400

    Add barrage event histogram

--- ProcessCellMetrics.m
+++ ProcessCellMetrics.m
@@ -1106,8 +1106,30 @@ if any(contains(parameters.metrics,{'event_metrics','all'})) && ~any(contains(pa
                 end
             end
         end
-
     end
+    
+    if exist([basepath '\Barrage_Files\' basename '.HSE.mat'])
+        load([basepath '\Barrage_Files\' basename '.HSE.mat'])
+        barrage.timestamps = []; barrage.peaks = [];
+        barrage.timestamps = HSE.timestamps(HSE.keep(HSE.NREM),:);
+        barrage.peaks = HSE.peaks(HSE.keep(HSE.NREM));
+        if ~isempty(barrage.peaks)
+            % Barrage specific histogram
+            PSTH = calc_PSTH(barrage,spikes{spkExclu},'alignment','onset','duration',0.5,'binCount',150,'smoothing',5,'eventName','barrage','plots',parameters.showFigures);
+            if size(PSTH.responsecurve,2) == cell_metrics.general.cellCount
+                cell_metrics.events.barrage = num2cell(PSTH.responsecurve,1);
+                cell_metrics.general.events.barrage.event_file = ['\Barrage_Files\' basename '.HSE.mat'];
+                cell_metrics.general.events.barrage.x_bins = PSTH.time*1000;
+                cell_metrics.general.events.barrage.x_label = 'Time (ms)';
+                cell_metrics.general.events.barrage.alignment = PSTH.alignment;
+                
+                cell_metrics.(['barrage','_modulationIndex']) = PSTH.modulationIndex;
+                cell_metrics.(['barrage','_modulationPeakResponseTime']) = PSTH.modulationPeakResponseTime;
+                cell_metrics.(['barrage','_modulationSignificanceLevel']) = PSTH.modulationSignificanceLevel;
+            end
+        end
+    end
+    
 end
 
 %% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
