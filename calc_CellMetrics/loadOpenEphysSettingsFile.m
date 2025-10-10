76beccb175fd7e28554595b5d70e62460e0d526e
Peter C. Petersen <petersen.peter@gmail.com>
2025-10-10 13:28:02 +0200

    Update loadOpenEphysSettingsFile.m

--- calc_CellMetrics/loadOpenEphysSettingsFile.m
+++ calc_CellMetrics/loadOpenEphysSettingsFile.m
@@ -5,13 +5,13 @@ function session = loadOpenEphysSettingsFile(file1,session,varargin)
 % https://open-ephys.github.io/gui-docs/User-Manual/Recording-data/Binary-format.html
 
 p = inputParser;
-addParameter(p,'probe_letter','A',@ischar);
+addParameter(p,'probeLetter','A',@ischar);
 
 % Parsing inputs
 parse(p,varargin{:})
 parameters = p.Results;
 
-switch parameters.probe_letter
+switch parameters.probeLetter
     case 'A'
         stream_id = 1;
     case 'B'
@@ -100,6 +100,88 @@ if exist(file1,'file')
         session.timeSeries.dig.equipment = 'OpenEpys Neuropix-PXI';
         session.timeSeries.dig.nSamples = [];
         session.timeSeries.dig.leastSignificantBit = [];
+    end
+
+
+    if strcmp(openEphys_metadata.GUIVersion, '1.0.1')
+        % Importing metadata
+        session.extracellular.sr = openEphys_metadata.continuous(stream_id).sample_rate;
+        session.extracellular.nChannels = openEphys_metadata.continuous(stream_id).num_channels;
+        session.extracellular.equipment = 'Open Ephys Neuropix-PXI'; % Fixed typo: "Epys" -> "Ephys"
+        session.extracellular.leastSignificantBit = 0.195;
+        session.extracellular.fileFormat = 'dat';
+        session.extracellular.precision = 'int16';
+        session.extracellular.fileName = '';
+        
+        % Electrode groups and channel mapping
+        channelmapping = zeros(1, session.extracellular.nChannels); % Pre-allocate for efficiency
+        for i = 1:session.extracellular.nChannels
+            if isstruct(openEphys_metadata.continuous(stream_id).channels) 
+                if isfield(openEphys_metadata.continuous(stream_id).channels(i),'channel_metadata')
+                    % REMOVED +1: The metadata value is now correctly 0-indexed and matches CH0, CH1, etc.
+                    channelmapping(i) = openEphys_metadata.continuous(stream_id).channels(i).channel_metadata.value;
+                end
+            elseif isfield(openEphys_metadata.continuous(stream_id).channels{i},'channel_metadata')
+                % REMOVED +1: The metadata value is now correctly 0-indexed and matches CH0, CH1, etc.
+                channelmapping(i) = openEphys_metadata.continuous(stream_id).channels{i}.channel_metadata.value;
+            end
+        end
+    
+        chanCoords = generateChanCoords_Neuropixel2;
+    
+        session.extracellular.chanCoords = chanCoords;
+        session.extracellular.chanCoords.x = chanCoords.x(channelmapping + 1); % Add 1 here if generateChanCoords_Neuropixel2 is 1-indexed
+        session.extracellular.chanCoords.y = chanCoords.y(channelmapping + 1);
+        session.extracellular.chanCoords.shank = chanCoords.shank(channelmapping + 1);
+        session.extracellular.chanCoords.channels = chanCoords.channels(channelmapping + 1);
+    
+        session.extracellular.electrodeGroups.channels = {};
+        
+        shanks = unique(session.extracellular.chanCoords.shank);
+        for j = 1:length(shanks)
+            session.extracellular.electrodeGroups.channels{j} = find(session.extracellular.chanCoords.shank == shanks(j));
+            [~,idx] = sortrows([...
+                session.extracellular.chanCoords.x(session.extracellular.electrodeGroups.channels{j});...
+                session.extracellular.chanCoords.y(session.extracellular.electrodeGroups.channels{j})...
+                ]',[2,1],{'descend','ascend'});
+            session.extracellular.electrodeGroups.channels{j} = session.extracellular.electrodeGroups.channels{j}(idx);
+            session.extracellular.electrodeGroups.label{j} = ['shanks',num2str(shanks(j))]; 
+        end
+        session.extracellular.nElectrodeGroups = length(shanks);
+    
+        figure
+        plot(chanCoords.x,chanCoords.y,'.k'), hold on
+        site_cmap = hot(session.extracellular.nElectrodeGroups);
+        for j = 1:session.extracellular.nElectrodeGroups
+            x = session.extracellular.chanCoords.x(session.extracellular.electrodeGroups.channels{j});
+            y = session.extracellular.chanCoords.y(session.extracellular.electrodeGroups.channels{j});
+            plot(x,y,'s',MarkerFaceColor=site_cmap(j,:))
+        end
+        xlabel('X position (µm)'), ylabel('Y position (µm)'), title(['Neuropixel site selection: ', file1]) % Fixed title to show filename
+        axis equal
+    
+        session.extracellular.spikeGroups.channels = session.extracellular.electrodeGroups.channels;
+        session.extracellular.nSpikeGroups = session.extracellular.nElectrodeGroups;
+    
+        % LFP data stream
+        if length(openEphys_metadata.continuous)>1
+            session.extracellular.srLfp = openEphys_metadata.continuous(2).sample_rate;
+        end
+    
+        % Loading events from timestamps
+        % Check if events array is not empty
+        if ~isempty(openEphys_metadata.events)
+            session.timeSeries.dig.fileName = [openEphys_metadata.events{1}.folder_name,'timestamps.npy'];
+            session.timeSeries.dig.fileFormat = 'npy';
+            session.timeSeries.dig.precision  = openEphys_metadata.events{1}.type;
+            session.timeSeries.dig.nChannels = 1;
+            session.timeSeries.dig.sr = openEphys_metadata.events{1}.sample_rate;
+            session.timeSeries.dig.equipment = 'Open Ephys Neuropix-PXI'; % Fixed typo
+            session.timeSeries.dig.nSamples = [];
+            session.timeSeries.dig.leastSignificantBit = [];
+        else
+            warning('No events found in the structure.oebin file.');
+        end
     end
 else
     warning(['Failed to load Open Ephys settings: ', file1])
